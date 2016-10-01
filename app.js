208060c12c2eaca87d36e320c0474485b9a062ce
Jon Small <jonssmall@gmail.com>
2016-10-01 16:41:21 -0400

    basic web socket server

---
+++ app.js
@@ -0,0 +1,53 @@
+var WebSocketServer = require("ws").Server
+var http = require("http")
+var express = require("express")
+var app = express()
+var port = process.env.PORT || 5000
+
+app.use(express.static(__dirname + "/"))
+
+
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/index.html');
+});
+
+app.get('/groceries', function (req, res) {	
+	var listString = req.query.list;
+	var listArray = listString.split(',')
+	var listObject = {}
+	for(var i = 0; i < listArray.length; i++) {
+		listObject[i] = listArray[i];
+	}
+	wss.broadcast(listObject);
+  	res.send(listObject); 	
+});
+
+var server = http.createServer(app)
+server.listen(process.env.port || port);
+
+console.log("http server listening");
+
+var wss = new WebSocketServer({server: server})
+wss.broadcast = function broadcast(data) {
+	console.log(typeof data);
+	console.log(typeof data[0]);
+	wss.clients.forEach(function each(client) {		
+		client.send(JSON.stringify(data));
+	});
+};
+
+console.log("websocket server created")
+
+wss.on("connection", function(ws) {	
+    all_active_connections[id] = ws;     
+           
+	ws.on("close", function() {		
+	    delete all_active_connections[ws.id];	    
+	    console.log("connection closed");
+        console.log(all_active_connections);
+	});
+
+	ws.on("error", function() {		
+	    delete all_active_connections[ws.id];	    	    
+	});
+});
